2834362b5a00d478031f0445bddf367ea7d518db
ariabrauchli <42819915+ariabrauchli@users.noreply.github.com>
2025-10-14 17:49:10 -0600

    recipe names

--- src/components/Recipecard.jsx
+++ src/components/Recipecard.jsx
@@ -1,8 +1,69 @@
 function RecipeCard() {
   return (
-    <div id="recipe-card">
-      <h3>Recipe Title</h3>
-      <p>Recipe description goes here...</p>
+    <div>
+      <div id="recipe-card">
+        <h3>Katsudon</h3>
+        <h4>Ingredients</h4>
+        <p></p>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
+      <div id="recipe-card">
+        <h3>Creamy Bay Scallop Spaghetti</h3>
+        <h4>Ingredients</h4>
+        <p></p>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
+      <div id="recipe-card">
+        <h3>Better Boxed Vegan Cake</h3>
+        <h4>Ingredients</h4>
+        <p></p>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
+      <div id="recipe-card">
+        <h3>Cinnamon Rolls</h3>
+        <h4>Ingredients</h4>
+        <p></p>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
+      <div id="recipe-card">
+        <h3>Olive Garden Zuppa Toscana Soup</h3>
+        <h4>Ingredients</h4>
+        <p></p>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
+      <div id="recipe-card">
+        <h3>Homemade Hamburger Helper Beef Noodle</h3>
+        <h4>Ingredients</h4>
+        <p></p>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
+      <div id="recipe-card">
+        <h3>Chicken Fried Rice</h3>
+        <h4>Ingredients</h4>
+        <p></p>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
+      <div id="recipe-card">
+        <h3>Egg Drop Soup</h3>
+        <h4>Ingredients</h4>
+        <p></p>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
+      <div id="recipe-card">
+        <h3>Chocolate Chip Cookies</h3>
+        <h4>Ingredients</h4>
+        <p></p>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
     </div>
   )
 }
