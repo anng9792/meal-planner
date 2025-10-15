1299b60c9294a29241b1fe2fdf649607bc715ffd
ariabrauchli <42819915+ariabrauchli@users.noreply.github.com>
2025-10-14 18:49:36 -0600

    recipe

--- src/components/Recipecard.jsx
+++ src/components/Recipecard.jsx
@@ -4,47 +4,157 @@ function RecipeCard() {
       <div id="recipe-card">
         <h3>Katsudon</h3>
         <h4>Ingredients</h4>
-        <ul></ul>
-        <h4>Instructions</h4>
-        <p>Recipe description goes here...</p>
-      </div>
-      <div id="recipe-card">
-        <h3>Creamy Bay Scallop Spaghetti</h3>
-        <h4>Ingredients</h4>
         <ul>
-
+          <li>2 Pork shoulder/loin steak or filleted chicken breast/thigh (150g)</li>
+          <li>1 white onion, thinly sliced</li>
+          <li>1 egg</li>
+          <li>2 cups cooked white rice</li>
+          <li>chives for garnish</li>
+          <li>1 cup All Purpose flour</li>
+          <li>2 cups panko breadcrumbs</li>
+          <li>Neutral oil for frying</li>
+        </ul>
+        <h5>Batter</h5>
+        <ul>
+          <li>½ cup All Purpose flour</li>
+          <li>½ cup water</li>
+          <li>1 egg</li>
+          <li>1 tsp salt </li>
+        </ul>
+        <h5>Sauce</h5>
+        <ul>
+          <li>1 cup stock (dashi, chicken, veggie all works!)</li>
+          <li>2 tablespoon sake</li>
+          <li>2 tablespoon soy sauce</li>
+          <li>2 tablespoon mirin</li>
+          <li>2 teaspoon sugar</li>
         </ul>
         <h4>Instructions</h4>
         <p>Recipe description goes here...</p>
       </div>
+
       <div id="recipe-card">
-        <h3>Better Boxed Vegan Cake</h3>
+        <h3>Creamy Bay Scallop Spaghetti</h3>
+        <p>Recipe description goes here...</p>
         <h4>Ingredients</h4>
-        <ul></ul>
+        <ul>
+          <li>8 ounces uncooked thick spaghetti</li>
+          <li>1 tablespoon vegetable oil</li>
+          <li>1 pound bay scallops</li>
+          <li>2 tablespoons butter</li>
+          <li>3 cloves garlic, minced</li>
+          <li>2 teaspoons grated lemon zest</li>
+          <li>1 pinch red pepper flakes</li>
+          <li>⅓ cup dry sherry</li>
+          <li>1 cup heavy cream</li>
+          <li>salt and pepper to taste</li>
+          <li>1 lemon, juiced</li>
+          <li>2 tablespoons chopped fresh parsley</li>
+          <li>freshly grated Parmigiano-Reggiano cheese, for serving</li>
+        </ul>
         <h4>Instructions</h4>
         <p>Recipe description goes here...</p>
       </div>
+
+      <div id="recipe-card">
+        <h3>Better Boxed Vegan Cake</h3>
+        <p>Recipe description goes here...</p>
+        <h4>Ingredients</h4>
+        <ul>
+          <li>1 box cake mix </li>
+          <li>⅔ cup vegan yogurt (5 oz) or vegan sour cream</li>
+          <li>⅔ cup vegetable oil</li>
+          <li>¾ cup vegan milk</li>
+          <li>1 teaspoon lemon juice, apple cider or white vinegar</li>
+          <li>1 tablespoon cornstarch</li>
+        </ul>
+        <h4>Instructions</h4>
+        <p>Recipe description goes here...</p>
+      </div>
+
       <div id="recipe-card">
         <h3>Cinnamon Rolls</h3>
         <h4>Ingredients</h4>
-        <ul></ul>
+        <h5>Dough Ingredients</h5>
+        <ul>
+          <li>½ cup salted butter, melted</li>
+          <li>1 ½ cups whole milk</li>
+          <li>6 ½ cups all-purpose flour, divided</li>
+          <li>2 (¼ ounce) packets active dry yeast</li>
+          <li>½ cup granulated sugar</li>
+          <li>½ cup room temperature- water</li>
+          <li>1 teaspoon salt</li>
+          <li>2 large eggs</li>
+        </ul>
+        <h5>Filling Ingredients</h5>
+        <ul>
+          <li>2 cups light brown sugar</li>
+          <li>2 tablespoons ground cinnamon</li>
+          <li>¼ cup salted butter, melted</li>
+          <li>1 cup salted butter, softened</li>
+        </ul>
+        <h5>Cream Cheese Frosting Ingredients</h5>
+        <ul>
+          <li>8 ounces cream cheese</li>
+          <li>¼ cup salted butter, softened</li>
+          <li>1 ½ cups powdered sugar</li>
+          <li>1 teaspoon vanilla extract</li>
+          <li>3 tablespoons heavy cream</li>
+          <li>⅛ teaspoon salt</li>
+        </ul>
+        <h5>Butterscotch Topping Ingredients</h5>
+        <ul>
+          <li>½ cup light brown sugar</li>
+          <li>4 tablespoons salted butter, softened</li>
+          <li>½ cup heavy cream</li>
+          <li>1 teaspoon vanilla extract</li>
+          <li>¼ teaspoon salt</li>
+        </ul>
         <h4>Instructions</h4>
         <p>Recipe description goes here...</p>
       </div>
+
       <div id="recipe-card">
         <h3>Olive Garden Zuppa Toscana Soup</h3>
         <h4>Ingredients</h4>
-        <ul></ul>
+        <ul>
+          <li>1 lb Italian ground sausage</li>
+          <li>4 tablespoons butter</li>
+          <li>½ white onion, diced</li>
+          <li>1 tablespoon minced garlic</li>
+          <li>6 cups chicken broth</li>
+          <li>2 cups water</li>
+          <li>4-5 yellow potatoes, cut into 1-inch pieces</li>
+          <li>3 teaspoons salt</li>
+          <li>1 teaspoon black pepper</li>
+          <li>2 cups heavy cream</li>
+          <li>4 cups chopped kale</li>
+          <li>chopped bacon or bacon bits and grated parmesan cheese for topping</li>
+        </ul>
         <h4>Instructions</h4>
         <p>Recipe description goes here...</p>
       </div>
+
       <div id="recipe-card">
         <h3>Homemade Hamburger Helper Beef Noodle</h3>
         <h4>Ingredients</h4>
-        <ul></ul>
+        <ul>
+          <li>1 lb ground beef</li>
+          <li>2 cups wide egg noodles</li>
+          <li>1 ½ cups water</li>
+          <li>1 ½ cups milk</li>
+          <li>3 beef bouillon cubes</li>
+          <li>1 tablespoon cornstarch</li>
+          <li>1 teaspoon garlic powder</li>
+          <li>½ teaspoon onion powder</li>
+          <li>1 teaspoon sugar</li>
+          <li>1 teaspoon salt</li>
+          <li>½ teaspoon black pepper</li>
+        </ul>
         <h4>Instructions</h4>
         <p>Recipe description goes here...</p>
       </div>
+
       <div id="recipe-card">
         <h3>Chicken Fried Rice</h3>
         <h4>Ingredients</h4>
@@ -52,6 +162,7 @@ function RecipeCard() {
         <h4>Instructions</h4>
         <p>Recipe description goes here...</p>
       </div>
+
       <div id="recipe-card">
         <h3>Egg Drop Soup</h3>
         <h4>Ingredients</h4>
@@ -59,6 +170,7 @@ function RecipeCard() {
         <h4>Instructions</h4>
         <p>Recipe description goes here...</p>
       </div>
+
       <div id="recipe-card">
         <h3>Chocolate Chip Cookies</h3>
         <h4>Ingredients</h4>
