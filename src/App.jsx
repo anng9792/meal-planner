33bc6516f0617414846abc95ccb6f0aa31058a30
Angelli Nguyen <anng9792@colorado.edu>
2025-10-16 16:12:03 -0600

    gallery css

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,48 @@ import { useState } from 'react'
 import Searchbar from './components/Searchbar'
 import Gallery from './components/Gallery'
 import RecipeCard from './components/RecipeCard'
-import { recipes } from './data/recipes'
 import './index.css'
 
 function App() {
-  const [results, setResults] = useState([])
+    const defaultRecipes = [
+    {
+      id: 1,
+      title: "Olive Garden Zuppa Toscana Soup",
+      image: "https://www.lecremedelacrumb.com/wp-content/uploads/2019/03/easy-zuppa-toscano-soup-3.jpg",
+      cookTime: "40 Mins",
+      type: "High Protein"
+    },
+
+    {
+      id: 2,
+      title: "Better Boxed Vegan Cake",
+      image: "https://bigboxvegan.com/wp-content/uploads/2023/04/IMG_5110.jpeg",
+      cookTime: "30 Mins",
+      type: "Vegan"
+    },
+
+    {
+      id: 3,
+      title: "Katsudon",
+      image: "https://doobydobap.com/wp-content/uploads/2021/08/IMG_0846-1688x2048.jpg",
+      cookTime: "15 Mins",
+    },
+
+    {
+      id: 4,
+      title: "Homemade Hamburger Helper Beef Noodle",
+      image: "https://cincyshopper.com/wp-content/uploads/2016/02/Homemade-Hamburger-Helper-Beef-Noodle-5.jpg",
+      cookTime: "30 Mins",
+    },
+  ];
+  const [results, setResults] = useState([defaultRecipes])
   
   const handleSearch = ({ query, ingredients, filters }) => {
     console.log("Searching for:", query)
     console.log("With ingredients", ingredients)
     console.log("Filters:", filters)
 
-    let filtered = recipes
+    let filtered = defaultRecipes
 
     if (query) {
       filtered = filtered.filter(r =>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,38 +1,20 @@
-function Gallery() {
-  const recipies = [
-    {
-      id: 1,
-      title: "Olive Garden Zuppa Toscana Soup",
-      image: "https://www.lecremedelacrumb.com/wp-content/uploads/2019/03/easy-zuppa-toscano-soup-3.jpg",
-      cookTime: "40 Mins",
-    },
-
-    {
-      id: 2,
-      title: "Better Boxed Vegan Cake",
-      image: "https://bigboxvegan.com/wp-content/uploads/2023/04/IMG_5110.jpeg",
-      cookTime: "30 Mins",
-    },
-
-    {
-      id: 3,
-      title: "Katsudon",
-      image: "https://doobydobap.com/wp-content/uploads/2021/08/IMG_0846-1688x2048.jpg",
-      cookTime: "15 Mins",
-    },
-
-    {
-      id: 4,
-      title: "Homemade Hamburger Helper Beef Noodle",
-      image: "https://cincyshopper.com/wp-content/uploads/2016/02/Homemade-Hamburger-Helper-Beef-Noodle-5.jpg",
-      cookTime: "30 Mins",
-    },
-  ];
-  
+function Gallery({ results }) {
   return (
-    <div id="gallery">
-      <h2>Gallery</h2>
-      <p>Recipes will show here...</p>
+    <div id="gallery" className="gallery">
+      <h2 className="gallery_title">Gallery</h2>
+      <div id="gallery_grid" className="gallery_grid">
+        {results.map((recipe) => (
+          <div key={recipe.id} className="gallery_card">
+            <img
+              src={recipe.image}
+              alt={recipe.title}
+              className="gallery_image"
+            />
+            <h3 className="card-title">{recipe.title}</h3>
+            <p className="cooktime">⏱ {recipe.cookTime}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
